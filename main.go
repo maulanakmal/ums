3eb0ddd247509448e07f72413fe6262ec7be7f33
Maulana Akmal <maulana2904@gmail.com>
2020-06-25 23:37:33 +0700

    clean up

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"./rpc"
 	"log"
 )
 
 func main() {
-	server := &Server{Port: "6000"}
+	server := &rpc.Server{Port: "6000"}
 
 	log.Printf("serving on port %s", server.Port)
 	server.ListenAndServe()
--- rpc/go.mod
+++ rpc/go.mod
@@ -1,3 +1,8 @@
 module mol/rpc
 
 go 1.14
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+)
