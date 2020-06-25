c55a8caf07f9f2b009f679d76042646ae26e6b6c
Maulana Akmal <maulana2904@gmail.com>
2020-06-25 23:18:37 +0700

    clean client code in main

--- main.go
+++ main.go
@@ -1,41 +1,12 @@
 package main
 
 import (
-	"encoding/gob"
 	"log"
-	"net"
 )
-
-type LoginRequest struct {
-	Username string
-	Password string
-}
-
-type RegisterRequest struct {
-	Username string
-	Nickname string
-	Password string
-}
-
-type ChangeNicknameRequest struct {
-	Username string
-	Nickname string
-}
 
 func main() {
 	server := &Server{Port: "6000"}
-	go server.ListenAndServe()
 
-	conn, err := net.Dial("tcp", "localhost:6000")
-	if err != nil {
-		panic(err.Error())
-	}
-	encoder := gob.NewEncoder(conn)
-	decoder := gob.NewDecoder(conn)
-	request := Request{Name: "changeNickname", Args: []string{"mol2", "molana"}}
-	var response Response
-	encoder.Encode(request)
-	decoder.Decode(&response)
-	log.Printf("response %q", response)
-
+	log.Printf("serving on port %s", server.Port)
+	server.ListenAndServe()
 }
